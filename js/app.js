86def1fbb3341dbef54d356f57b461981bedc83c
Lyubima Toshkova <lyubimatoshkova18@gmail.com>
2018-06-13 16:44:39 -0500

    feat:Added click,match and don't-match functionality

--- js/app.js
+++ js/app.js
@@ -18,12 +18,12 @@ const cards = ['fa-diamond','fa-diamond',
  *   - add each card's HTML to the page
  */
 function createCard(card) {
-    return `<li class="card"><i class="fa ${card}"></i></li>`
+    return `<li class="card" data-card = "${card}"><i class="fa ${card}"></i></li>`
 }
 
 function displayCards() {
     const deck = document.querySelector('.deck');
-    let cardHTML = cards.map(function(card){
+    let cardHTML = shuffle(cards).map(function(card){
         return createCard(card);
     });
 
@@ -47,14 +47,56 @@ function shuffle(array) {
     return array;
 }
 
+const allCards = document.querySelectorAll('.card');
+let openCards = [];
 
+/* 
+    set up the event listener for a card. If a card is clicked:
+   - display the card's symbol (put this functionality in another function that you call from this one)
+   - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one) 
+*/
+
+allCards.forEach(function(card) {
+    card.addEventListener('click', function(event) {
+        if(!card.classList.contains('open') && !card.classList.contains('show') && !card.classList.contains('match') ){
+            openCards.push(card);
+            card.classList.add('open','show');
+/* 
+    - if the list already has another card, check to see if the two cards match
+    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one) 
+*/
+            if(openCards.length ==2) {
+                if(openCards[0].dataset.card ==openCards[1].dataset.card) {
+                    openCards[0].classList.add('match');
+                    openCards[0].classList.add('open');
+                    openCards[0].classList.add('show');
+
+                    openCards[1].classList.add('match');
+                    openCards[1].classList.add('open');
+                    openCards[1].classList.add('show');
+
+                    openCards = [];
+                } else {
 /*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
+    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
+*/ 
+                    setTimeout(function() {
+                        openCards.forEach(function(card){
+                            card.classList.remove('open','show');
+                        });
+
+                        openCards = [];
+                    },1000
+
+                    );
+                }
+            }
+        }
+    });
+});
+
+
+ 
+ /*    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
