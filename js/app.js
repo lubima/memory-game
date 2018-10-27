e8598a398668f8f69f714245e22c626bc2b5507c
Lyubima Toshkova <lyubimatoshkova18@gmail.com>
2018-10-27 13:18:37 -0500

    feat:Added initial modal functionality

--- js/app.js
+++ js/app.js
@@ -57,6 +57,33 @@ function openWindow(message,moves,minutes,seconds) {
     let html = `<div style="font-size:30px">${message} * Your moves were:${moves} * Your time was: ${minutes} minutes and ${seconds} seconds</div>`; newWindow.document.body.insertAdjacentHTML('afterbegin', html);
 
 }
+//*******Modal******** 
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+//********* 
 
 const allCards = document.querySelectorAll('.card');
 // const starsHTML = document.getElementsByClassName('stars');
